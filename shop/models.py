5bfe240fe309d0807b3a27155d894232453312c6
sagar rajput <hiisa@Lenovo.TeleperformanceDIBS.com>
2026-03-24 21:35:55 +0530

    update product model and etc field

--- shop/models.py
+++ shop/models.py
@@ -6,4 +6,11 @@ class Product(models.Model):
     p_id = models.AutoField(primary_key=True)
     p_name = models.CharField(max_length=100)
     p_desc = models.CharField(max_length=300)
+    p_price  = models.IntegerField(default=0)
+    p_image = models.ImageField(upload_to="shop/images",default="")
+    p_cat = models.CharField(max_length=50,default="")
+    p_sub_cat = models.CharField(max_length=50,default="")
     pub_date = models.DateField()
+
+    def __str__(self):
+        return self.p_name
